72de84d6c995695c741a4522b5123acc2982ddc3
Remigiusz Piwowarski <224099@edu.p.lodz.pl>
2020-11-02 23:30:07 +0100

    First commit

---
+++ Task1/Task1/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class Customer
+    {
+        private string name;
+        private string surname;
+        private int phoneNumber;
+    }
+}
