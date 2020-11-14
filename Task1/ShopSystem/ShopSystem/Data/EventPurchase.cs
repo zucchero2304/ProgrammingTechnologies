61345d267bd62775b368da9fe88faeb98784fcc5
Remigiusz Piwowarski <224099@edu.p.lodz.pl>
2020-11-14 22:08:13 +0100

    Added enum category of products, created tests file

---
+++ Task1/ShopSystem/ShopSystem/Data/EventPurchase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopSystem.Data
+{
+    class EventPurchase : IEvent 
+    {
+        public EventPurchase(State state, int clientId, int id) 
+            : base(state, clientId, id) { }
+    }
+}
